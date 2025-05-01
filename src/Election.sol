bd37e848542d8e07e75f5f102740f6efdabef406
Ayeni-yeniyan <connectwithayeni@gmail.com>
2025-05-01 14:59:02 +0100

    forge init vuts

---
+++ src/Election.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Election {
+    // Election owner
+    // Election owner picks the election start time and end time and format
+    // Registration officer address
+    // Registration officers can register users by associating their address with users
+    // Check if user is valid
+    // Accredited voters can vote only
+    // Can view all voters
+    // Can check accredited voters
+    // Check if election is open
+    // Close election How should the previous result be stored?
+    // Election name
+    // Each election is a controct. When it is finished it can no longer be changed. Once done it is done
+    // Election start, OPEN_FOR_REGISTRATION, OPEN_FOR_VOTING, CLOSED
+}
