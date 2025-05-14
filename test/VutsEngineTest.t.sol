7f9551fb5ab13644c76f8deb4d55ba1fa678c428
Ayeni-yeniyan <connectwithayeni@gmail.com>
2025-05-14 03:54:12 +0100

    chore: test set up

---
+++ test/VutsEngineTest.t.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {VutsEngine} from "../src/VutsEngine.sol";
+import {DeployVutsEngine} from "../script/DeployVutsEngine.s.sol";
+
+contract VutsEngineTest is Test {
+    VutsEngine public vutsEngine;
+    DeployVutsEngine public deployVutsEngine;
+
+    function setUp() public {
+        deployVutsEngine = new DeployVutsEngine();
+        vutsEngine = deployVutsEngine.run();
+    }
+}
